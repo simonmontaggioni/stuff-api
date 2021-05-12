f1ad69a00ea22d5c30a44a9f283dcf305482b799
simon.montaggioni <simon.montaggioni@gmail.com>
2021-05-12 18:06:24 -0400

    feat: Create a POST route to retrieve data from the client

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -13,6 +16,11 @@ app.use((req, res, next) => {
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
+});
+
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({ message: "Thing created successfully" });
 });
 
 app.use("/api/stuff", (req, res, next) => {
