85873a9e857deb473de85bc6ae50331a7721c1fb
simon.montaggioni <simon.montaggioni@gmail.com>
2021-05-13 16:31:04 -0400

    refactor: Improve the use of the environment variables
      - Split the database name
      - Clear database host url parameters

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const { DB_HOST, DB_USER, DB_PASSWORD } = require("./config");
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = require("./config");
 const express = require("express");
 const mongoose = require("mongoose");
+const Thing = require("./models/thing");
 
 const app = express();
 
 mongoose
-  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`)
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`)
   .then(() => {
     console.log("Successfully connected to MongoDB Atlas");
   })
--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = {
   HOST: process.env.HOST || "127.0.0.1",
   PORT: process.env.PORT || 3000,
   DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
 };
