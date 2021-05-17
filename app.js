1edd46de086bacb66cfd030ed6825c81ebef1562
simon.montaggioni <simon.montaggioni@gmail.com>
2021-05-17 10:34:59 -0400

    feat: Enable the API to receive images from the client
      - Refactor the stuff controller to store the image url propertly
      - Refactor the app.js to serve static files from images folder
      - Modify the stuff routes to use the multer middleware
      - Add the images folder to the .gitignore
      - Create the .gitkeep file to preserve the images folder
      - Fix the multer middleware to create the file name without an extra
        parentheses

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = require("./config");
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const stuffRoutes = require("./routes/stuff");
 const userRoutes = require("./routes/user");
 
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/stuff", stuffRoutes);
 app.use("/api/auth", userRoutes);
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -4,9 +4,10 @@ const router = express.Router();
 
 const stuffController = require("../controllers/stuff");
 const authMiddleware = require("../middlewares/auth");
+const multerMiddleware = require("../middlewares/multer-config");
 
 router.get("/", authMiddleware, stuffController.getAllStuff);
-router.post("/", authMiddleware, stuffController.createThing);
+router.post("/", authMiddleware, multerMiddleware, stuffController.createThing);
 router.get("/:id", authMiddleware, stuffController.getOneThingById);
 router.put("/:id", authMiddleware, stuffController.updateThing);
 router.delete("/:id", authMiddleware, stuffController.deleteThing);
